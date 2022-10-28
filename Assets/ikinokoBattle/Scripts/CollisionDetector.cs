79465032f112a9d70dc2b1fd9aa74fa412c5c3cc
HMasataka <siruba.2050@gmail.com>
2022-10-28 15:07:38 +0900

    Feat chase enemy

---
+++ Assets/ikinokoBattle/Scripts/CollisionDetector.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8390506e3d5e1b864e3c4e4f66a7ab2805b941e3e391d414ef6de8a20c28af65
+size 409
--- Assets/ikinokoBattle/Scripts/EnemyMove.cs
+++ Assets/ikinokoBattle/Scripts/EnemyMove.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11aeb2b4fd49b974d07bd798954d7d6c1352e485b27f4cb2276a24fc79bded9c
-size 459
+oid sha256:a1e717c64b3f99d7977702cd1488f87b386a1eb5f6324011a32f0337fbcc07a7
+size 435
